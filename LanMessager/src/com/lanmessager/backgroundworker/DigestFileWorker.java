7adfcc53da3ee730ddb6d8757cbbdaae4f838662
Leonardo_Cosmos <kylin_cl@163.com>
2016-03-28 17:57:11 +0800

    Refactor: FileDigestCalculator
    FileDigestCalculator support multiple task. DigestFileWorker just
    monitor background calculator.

--- LanMessager/src/com/lanmessager/backgroundworker/DigestFileWorker.java
+++ LanMessager/src/com/lanmessager/backgroundworker/DigestFileWorker.java
@@ -1,12 +1,14 @@
 package com.lanmessager.backgroundworker;
 
 import java.io.File;
-import java.util.HashMap;
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.concurrent.CancellationException;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
 
 import javax.swing.SwingWorker;
 
@@ -17,8 +19,12 @@ import com.lanmessager.file.FileDigestResult;
 
 public class DigestFileWorker {
 	private static final Logger LOGGER = Logger.getLogger(DigestFileWorker.class.getSimpleName());
+		
+	private final FileDigestCalculator calculator;
 	
-	private Map<String, DigestFileSwingWorker> workerMap;
+	private final DigestFileMonitorSwingWorker monitor;
+	
+	private volatile boolean isMonitorShutdown = false;
 	
 	private Set<FileCompletedListener> completedListeners;
 
@@ -51,7 +57,9 @@ public class DigestFileWorker {
 	}
 	
 	public DigestFileWorker() {
-		workerMap = new HashMap<>();
+		this.calculator = new FileDigestCalculator();
+		this.monitor = new DigestFileMonitorSwingWorker();
+		this.monitor.execute();
 	}
 	
 	/**
@@ -62,53 +70,43 @@ public class DigestFileWorker {
 	 * @param file
 	 */
 	public void digest(String fileId, File file) {
-		DigestFileSwingWorker worker = new DigestFileSwingWorker(fileId, file);
-		
-		workerMap.put(fileId, worker);
-		
-		worker.execute();
+		calculator.digestFile(fileId, file);
 	}
 	
 	public void cancel(String fileId) {
-		if (!workerMap.containsKey(fileId)) {
-			LOGGER.warn("Cannot find file digest worker. File ID: " + fileId);
-			return;
-		}
+		calculator.cancel(fileId);
+	}
 		
-		DigestFileSwingWorker worker = workerMap.get(fileId);
-		worker.cancel();
-	}
-	
-	// XXX Merge complete trigger and rename.
-	protected void onCompleted(String fileId, FileDigestResult fileDigestResult) {
+	protected void onTaskDone(String fileId, Future<FileDigestResult> result) {	
 		if (completedListeners != null) {
 			FileCompletedEvent event = new FileCompletedEvent(this);
 			event.setFileId(fileId);
-			event.setFailed(false);
-			event.setFileDigestResult(fileDigestResult);
-			for (FileCompletedListener listener : completedListeners) {
-				listener.complete(event);
+			
+			if (!result.isDone()) {
+				LOGGER.warn("An unfinished task is reported: " + fileId);
+				return;
 			}
-		}
-	}
-	
-	protected void onCompletedByCancellation(String fileId) {
-		if (completedListeners != null) {
-			FileCompletedEvent event = new FileCompletedEvent(this);
-			event.setFileId(fileId);
-			event.setCanceled(true);
-			for (FileCompletedListener listener : completedListeners) {
-				listener.complete(event);
+			
+			if (result.isCancelled()) {
+				event.setCanceled(true);
+			} else {
+				try {
+					event.setFileDigestResult(result.get());
+				} catch (CancellationException ex) {
+					LOGGER.info("Task is canceled: " + fileId);
+					return;
+				} catch (InterruptedException ex) {
+					LOGGER.info("Task is interrupted: " + fileId);
+					return;
+				} catch (ExecutionException ex) {
+					event.setFailed(true);
+					event.setCause(ex.getCause());
+				} catch (Exception ex) {
+					event.setFailed(true);
+					event.setCause(ex);
+				}				
 			}
-		}
-	}
-	
-	protected void onCompletedByFailure(String fileId, Exception exception) {
-		if (completedListeners != null) {
-			FileCompletedEvent event = new FileCompletedEvent(this);
-			event.setFileId(fileId);
-			event.setFailed(true);
-			event.setCause(exception);
+			
 			for (FileCompletedListener listener : completedListeners) {
 				listener.complete(event);
 			}
@@ -127,62 +125,64 @@ public class DigestFileWorker {
 		}
 	}
 	
-	private class DigestFileSwingWorker extends SwingWorker<FileDigestResult, Long> {
-		private String fileId;
-		private File file;
-		private long fileLength;
-		private FileDigestCalculator calculator;
-		
-		public DigestFileSwingWorker(String fileId, File file) {
-			this.fileId = fileId;
-			this.file = file;
-			this.fileLength = file.length();
-		}
-
-		public void cancel() {
-			if (calculator != null) {
-				calculator.cancel();
-			}
-		}
+	private class DigestFileMonitorSwingWorker extends SwingWorker<Void, FileReport> {
+		private static final int REPORT_TIME_INTERVAL = 1000;
 		
 		@Override
-		protected FileDigestResult doInBackground() throws Exception {
+		protected Void doInBackground() throws Exception {
 			LOGGER.info("Digest file background thread starts.");
 			
-			calculator = new FileDigestCalculator();
-			calculator.addProgressUpdatedListener(event -> {
-				publish(event.getProcessed());
-			});
-			FileDigestResult result = calculator.digestFile(file);
+			while (!isMonitorShutdown) {				
+				Map<String, Future<FileDigestResult>> resultMap = calculator.reportResult();
+				Map<String, FileProgress> progressMap = calculator.reportProgress();
+				
+				List<FileReport> reportList = new ArrayList<>(resultMap.size() + progressMap.size()); 
+				resultMap.forEach((fileId, result) -> {
+					FileResultReport report = new FileResultReport();
+					report.setFileId(fileId);
+					report.setResult(result);
+					reportList.add(report);
+				});
+				progressMap.forEach((fileId, progress) -> {
+					FileProgressReport report = new FileProgressReport();
+					report.setFileId(fileId);
+					report.setProgress(progress);
+				});
+				
+				FileReport[] reports = new FileReport[reportList.size()];
+				reportList.toArray(reports);
+				publish(reports);
+				
+				Thread.sleep(REPORT_TIME_INTERVAL);
+			}			
 			
-			LOGGER.info("Digest file background thread exits.");
-			return result;
+			return null;
 		}
 		
 		@Override
-		protected void process(List<Long> chunks) {
+		protected void process(List<FileReport> chunks) {
 			super.process(chunks);
 			
-			long updatedLength = chunks.get(0);
-			onProgressUpdated(fileId, updatedLength, fileLength);
+			chunks.forEach(report -> {
+				if (report instanceof FileResultReport) {
+					FileResultReport resultReport = (FileResultReport) report;
+					onTaskDone(resultReport.getFileId(), resultReport.getResult());
+				} else if (report instanceof FileProgressReport) {
+					FileProgressReport progressReport = (FileProgressReport) report;
+					String fileId = progressReport.getFileId();				
+					FileProgress progress = progressReport.getProgress();
+					onProgressUpdated(fileId, progress.getProcessed(), progress.getTotal());
+				} else {
+					
+				}
+			});
 		}
 		
 		@Override
 		protected void done() {
 			super.done();
 			
-			workerMap.remove(fileId);
-			
-			if (calculator.isCanceled()) {
-				onCompletedByCancellation(fileId);
-			} else {				
-				try {
-					onCompleted(fileId, get());
-				} catch (InterruptedException | ExecutionException ex) {
-					onCompletedByFailure(fileId, ex);
-					LOGGER.error("Digest file background thread failed.", ex);
-				}				
-			}
+			LOGGER.info("Digest file background thread exits.");
 		}
 	}
 }
--- LanMessager/src/com/lanmessager/file/FileDigestCalculator.java
+++ LanMessager/src/com/lanmessager/file/FileDigestCalculator.java
@@ -4,74 +4,149 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
-import com.lanmessager.file.ProgressUpdatedEvent;
-import com.lanmessager.file.ProgressUpdatedListener;
+import org.apache.log4j.Logger;
+
+import com.lanmessager.backgroundworker.FileProgress;
 
 public class FileDigestCalculator {
-	private static final int BUFFER_LENGTH = 0x1000;
-	
-	private volatile boolean isCanceled = false;
-	
-	private Set<ProgressUpdatedListener> progressUpdatedListeners;
+	private Logger LOGGER = Logger.getLogger(FileDigestResult.class.getSimpleName());
 
-	public void addProgressUpdatedListener(ProgressUpdatedListener listener) {
-		if (progressUpdatedListeners == null) {
-			progressUpdatedListeners = new HashSet<>();
-		}
-		progressUpdatedListeners.add(listener);
+	private static final int BUFFER_LENGTH = 0x1000;
+
+	private final ExecutorService executor;
+
+	private final Map<String, Future<FileDigestResult>> resultMap;
+
+	private final Map<String, FileProgress> progressMap;
+
+	public FileDigestCalculator() {
+		executor = Executors.newCachedThreadPool();
+		resultMap = new HashMap<>();
+		progressMap = new HashMap<>();
 	}
 	
-	public void removeProgressUpdatedListener(ProgressUpdatedListener listener) {
-		if (progressUpdatedListeners != null) {
-			progressUpdatedListeners.remove(listener);
+	/**
+	 * Start digesting file asynchronously. 
+	 *
+	 */
+	public void digestFile(String fileId, File file) {
+		Future<FileDigestResult> result = executor.submit(new DigestFileTask(fileId, file));
+		synchronized (resultMap) {
+			if (resultMap.containsKey(fileId)) {
+				LOGGER.warn("Duplicated task: " + fileId);
+			}
+			resultMap.put(fileId, result);
 		}
 	}
 	
-	public FileDigestResult digestFile(File file) throws IOException {
+	public void cancel(String fileId) {
+		Future<FileDigestResult> result = null;
+		synchronized (resultMap) {
+			if (!resultMap.containsKey(fileId)) {
+				LOGGER.warn("Task doesn't exist: " + fileId);
+				return;
+			}
+			result = resultMap.get(fileId);
+		}
+		if (result != null) {
+			if (!result.cancel(true)) {
+				LOGGER.warn("Task cannot be canceled: " + fileId);
+			}
+		}
+	}
+	
+	/**
+	 * Get a map of results which have been done.
+	 * <p/>
+	 * After reported once, the result cannot be reported again 
+	 * and corresponding progress will not be reported.
+	 * 
+	 */
+	public Map<String, Future<FileDigestResult>> reportResult() {
+		Map<String, Future<FileDigestResult>> reportMap = new HashMap<>();
+		synchronized (resultMap) {
+			resultMap.forEach((id, result) -> {
+				if (result.isDone()) {
+					reportMap.put(id, result);
+				}
+			});
+			reportMap.forEach((id, result) -> resultMap.remove(id));
+		}
+		if (!reportMap.isEmpty()) {
+			synchronized (progressMap) {
+				reportMap.forEach((id, result) -> progressMap.remove(id));
+			}
+		}
+		return reportMap;
+	}
+	
+	/** Get a map of task progress.
+	 * 
+	 */
+	public Map<String, FileProgress> reportProgress() {
+		Map<String, FileProgress> reportMap = new HashMap<>();
+		synchronized (progressMap) {
+			progressMap.forEach((id, progress) -> reportMap.put(id, progress));
+		}
+		return reportMap;
+	}
+
+	protected void onProgressUpdated(String fileId, long processed, long total) {
+		synchronized (progressMap) {
+			FileProgress progress = new FileProgress();
+			progress.setProcessed(processed);
+			progress.setTotal(total);
+			progressMap.put(fileId, progress);	
+		}
+	}
+
+	private FileDigestResult calculate(String fileId, File file) throws IOException {
 		FileDigest fileDigest = FileDigest.getInstance();
 		InputStream input = null;
 		FileDigestResult result = null;
 		try {
 			input = new FileInputStream(file);
+			long fileLength = file.length();
 			byte[] buffer = new byte[BUFFER_LENGTH];
 			int readLength = 0;
 			long updatedLength = 0;
-			while (!isCanceled && (readLength = input.read(buffer, 0, BUFFER_LENGTH)) > 0) {
+			while ((readLength = input.read(buffer, 0, BUFFER_LENGTH)) > 0) {
 				fileDigest.update(buffer, 0, readLength);
-				
+
 				updatedLength += readLength;
-				onProgressUpdated(updatedLength);
+				LOGGER.debug("Calculate " + updatedLength + " bytes (total " + fileLength + " bytes).");
+				
+				onProgressUpdated(fileId, updatedLength, fileLength);
 			}
-			
-			if (!isCanceled) {
-				result = fileDigest.digest();
-			}
+
+			result = fileDigest.digest();
 		} finally {
 			if (input != null) {
 				input.close();
 			}
 		}
 		return result;
-	}	
-
-	public void cancel() {
-		isCanceled = true;
 	}
 	
-	public boolean isCanceled() {
-		return isCanceled;
-	}
-
-	protected void onProgressUpdated(long processed) {
-		if (progressUpdatedListeners != null) {
-			ProgressUpdatedEvent event = new ProgressUpdatedEvent(this);
-			event.setProcessed(processed);
-			for (ProgressUpdatedListener listener : progressUpdatedListeners) {
-				listener.updateProgress(event);
-			}
+	private class DigestFileTask implements Callable<FileDigestResult> {
+		private final String fileId;
+		private final File file;
+		
+		public DigestFileTask(String fileId, File file) {
+			this.fileId = fileId;
+			this.file = file;
+		}
+		
+		@Override
+		public FileDigestResult call() throws Exception {
+			return calculate(fileId, file);
 		}
 	}
 }
