e0762ebaf383c9fafd80369c80ebe2f420cffe4f
Leonardo_Cosmos <kylin_cl@163.com>
2016-03-29 18:06:53 +0800

    Delete deprecated class

--- LanMessager/src/com/lanmessager/communication/ProgressUpdatedListener.java
+++
@@ -1,7 +0,0 @@
-package com.lanmessager.communication;
-
-import java.util.EventListener;
-
-public interface ProgressUpdatedListener extends EventListener {
-	void updateProgress(ProgressUpdatedEvent e);
-}
