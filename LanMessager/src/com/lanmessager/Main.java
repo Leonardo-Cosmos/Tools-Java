e5339be513c567a6a897e42ab65276df43a0c25f
Leonardo_Cosmos <kylin_cl@163.com>
2016-05-12 16:35:46 +0800

    Improve: use system look and feel

--- LanMessager/src/com/lanmessager/Main.java
+++ LanMessager/src/com/lanmessager/Main.java
@@ -2,15 +2,27 @@ package com.lanmessager;
 
 import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
 
+import org.apache.log4j.Logger;
 import org.apache.log4j.PropertyConfigurator;
 
 import com.lanmessager.ui.MainFrame;
 
 public class Main {
 
+	private static final Logger LOGGER = Logger.getLogger(Main.class.getSimpleName());
+	
 	public static void main(String[] args) {
 		PropertyConfigurator.configure("log4j.properties");
+		
+		try {
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		} catch (ClassNotFoundException | InstantiationException | IllegalAccessException
+				| UnsupportedLookAndFeelException ex) {
+			LOGGER.error("Set look and feel failed.", ex);
+		}
 		
 		SwingUtilities.invokeLater(new Runnable() {
 			
